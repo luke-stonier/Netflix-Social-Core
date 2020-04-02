173779dde22d20d1187b4ea932a9f6f50e1f5aff
luke stonier <lukestonier@hotmail.com>
2020-04-02 18:12:29 +0100

    copy

--- party-core.js
+++ party-core.js
@@ -2,27 +2,27 @@ var http = require('http');
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 client.connect();
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) { console.log(err); return; }
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 
-var server = http.createServer(function (request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(200);
-    response.end();
-});
-server.listen(PORT, function () {
-    console.log((new Date()) + ' Server is listening on port ' + PORT);
-});
+// var server = http.createServer(function (request, response) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.writeHead(200);
+//     response.end();
+// });
+// server.listen(PORT, function () {
+//     console.log((new Date()) + ' Server is listening on port ' + PORT);
+// });
