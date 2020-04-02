25b4d49258d2271b69db4b9e9a75ff787153af2f
luke stonier <lukestonier@hotmail.com>
2020-04-02 18:06:22 +0100

    log error

--- party-core.js
+++ party-core.js
@@ -9,7 +9,7 @@ const client = new Client({
 client.connect();
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
+    if (err) { console.log(err); return; }
     for (let row of res.rows) {
         console.log(JSON.stringify(row));
     }
