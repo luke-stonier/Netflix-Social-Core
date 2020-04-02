798569b6f5bed8bfc3ce048eca3da6b886f47aee
luke stonier <lukestonier@hotmail.com>
2020-04-02 17:51:31 +0100

    connect to database

--- party-core.js
+++ party-core.js
@@ -1,4 +1,21 @@
 var http = require('http');
+const { client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
 const PORT = process.env.PORT || 3000
 
 var server = http.createServer(function (request, response) {
