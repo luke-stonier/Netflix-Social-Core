98cbd5dbad0353e6b0686d79cea7ecc8ce26d58e
luke stonier <lukestonier@hotmail.com>
2020-04-03 20:15:56 +0100

    started implimentaion for storage

--- party-core.js
+++ party-core.js
@@ -11,20 +11,20 @@ const client = new Client({
     ssl: true
 });
 
-app.get('/', (req, res) => {
-    client.query('SELECT * FROM GroupInstances;', (err, res) => {
-        if (err) { console.log("error: " + err); return; }
-        if (!res) { console.log("no result"); return; }
-    
-        console.log(`got ${res.rows.length} rows`);
-    
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-    
-        client.end();
-    });
-    
+app.get('/:groupName', async function(req, res) {
+    var groupName = req.params.groupName;
+    if (!groupName) { res.sendStatus(400); return; }
+    console.log(`Get group name info for ${groupName}`);
+    var sql = `SELECT * FROM GroupInstances WHERE GroupName = '${groupName}';`;
+    var rows = MakeSqlQuery(sql);
+    console.log(rows);
+
+    if (rows.length == 0) {
+        //CreateGroup(groupName);
+        res.sendStatus(204);
+        return;
+    }
+
     res.send();
 });
 
@@ -32,8 +32,44 @@ app.listen(PORT, () => {
     console.log((new Date()) + ' Server is listening on port ' + PORT);
 
     client.connect().then(() => {
-        console.log('Postgres client connected');
+        console.log('SQL client connected');
     }).catch((error) => {
         console.log(error);
     });
 });
+
+function CreateGroup(groupName) {
+    var serverAddress = "https://watch-hub.herokuapp.com/"
+    var sql = `INSERT INTO GroupInstances (GroupName, server) VALUES ('${groupName}', ${serverAddress});`;
+    var rows = MakeSqlQuery(sql);
+    console.log(rows);
+}
+
+function SetClientCount(groupName, clietnCount) {
+
+}
+
+function RemoveGroup(groupname) {
+    var sql = `DELETE FROM GroupInstances WHERE GroupName = '${groupname}';`;
+    var rows = MakeSqlQuery(sql);
+    console.log(rows);
+}
+
+function MakeSqlQuery(sql) {
+    if (!client) { return; }
+    client.query(sql, (err, res) => {
+        if (err) {
+            console.log("error: " + err);
+            return;
+        }
+
+        if (!res) {
+            console.log("no result");
+            return;
+        }
+    
+        client.end();
+
+        return res.rows;
+    });
+}
